93ee412ce797a6fefcb6e6bdbabefe0aafd9805b
Mandes Schoenherr <mandes.schoenherr@unibe.ch>
2021-03-25 15:54:16 +0100

    halo_exchange relocation and rm of release values

---
+++ checks/microbenchmarks/mpi/halo_exchange/halo_cell_exchange.py
@@ -0,0 +1,140 @@
+# Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.required_version('>=2.16.0-dev.0')
+@rfm.parameterized_test(*([a]
+                            for a in ['ivy', 'broadwell', 'amd']))
+class HaloCellExchangeTest(rfm.RegressionTest):
+    def __init__(self, arch):
+        self.curr_arch = arch
+        self.sourcepath = 'halo_cell_exchange.c'
+        self.build_system = 'SingleSource'
+        self.build_system.cflags = ['-O2']
+        self.valid_prog_environs = ['foss', 'intel']
+        self.executable_opts = ['input.txt']
+        self.valid_systems = [ ]
+
+        if self.curr_arch in ['amd']:
+           self.valid_systems = [ 'ubelix:amd' ]
+           self.num_tasks = 4
+           self.num_tasks_per_node = 1
+           self.num_gpus_per_node = 0
+           self.perf_patterns = {
+            'time_2_10': sn.extractsingle(
+                r'halo_cell_exchange 4 2 1 1 10 10 10'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+            'time_2_10000': sn.extractsingle(
+                r'halo_cell_exchange 4 2 1 1 10000 10000 10000'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+            'time_2_1000000': sn.extractsingle(
+                r'halo_cell_exchange 4 2 1 1 1000000 1000000 1000000'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+            'time_4_10': sn.extractsingle(
+                r'halo_cell_exchange 4 2 2 1 10 10 10'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+            'time_4_10000': sn.extractsingle(
+                r'halo_cell_exchange 4 2 2 1 10000 10000 10000'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+            'time_4_1000000': sn.extractsingle(
+                r'halo_cell_exchange 4 2 2 1 1000000 1000000 1000000'
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                'time_mpi', float),
+           }
+
+           self.sanity_patterns = sn.assert_eq(
+                sn.count(sn.findall(r'halo_cell_exchange', self.stdout)), 6)
+        elif self.curr_arch in ['ivy ', 'broadwell']:
+            self.valid_systems = [ 'ubelix:ivy', 'ubelix:broadwell' ]
+            self.num_tasks = 6
+            self.num_tasks_per_node = 1
+            self.num_gpus_per_node = 0
+            self.perf_patterns = {
+                'time_2_10': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 1 1 10 10 10'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_2_10000': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 1 1 10000 10000 10000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_2_1000000': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 1 1 1000000 1000000 1000000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_4_10': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 2 1 10 10 10'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_4_10000': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 2 1 10000 10000 10000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_4_1000000': sn.extractsingle(
+                    r'halo_cell_exchange 6 2 2 1 1000000 1000000 1000000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_6_10': sn.extractsingle(
+                    r'halo_cell_exchange 6 3 2 1 10 10 10'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_6_10000': sn.extractsingle(
+                    r'halo_cell_exchange 6 3 2 1 10000 10000 10000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float),
+                'time_6_1000000': sn.extractsingle(
+                    r'halo_cell_exchange 6 3 2 1 1000000 1000000 1000000'
+                    r' \S+ (?P<time_mpi>\S+)', self.stdout,
+                    'time_mpi', float)
+            }
+            self.sanity_patterns = sn.assert_eq(
+                sn.count(sn.findall(r'halo_cell_exchange', self.stdout)), 9)
+
+        self.reference = {
+            'ubelix:ivy': {
+                'time_2_10': (1, None, 0.50, 's'),
+                'time_2_10000': (1, None, 0.50, 's'),
+                'time_2_1000000': (1, None, 0.50, 's'),
+                'time_4_10': (1, None, 0.50, 's'),
+                'time_4_10000': (1, None, 0.50, 's'),
+                'time_4_1000000': (1, None, 0.50, 's'),
+                'time_6_10': (1, None, 0.50, 's'),
+                'time_6_10000': (1, None, 0.50, 's'),
+                'time_6_1000000': (1, None, 0.50, 's')
+            },
+           'ubelix:broadwell': {
+                'time_2_10': (1, None, 0.50, 's'),
+                'time_2_10000': (1, None, 0.50, 's'),
+                'time_2_1000000': (1, None, 0.50, 's'),
+                'time_4_10': (1, None, 0.50, 's'),
+                'time_4_10000': (1, None, 0.50, 's'),
+                'time_4_1000000': (1, None, 0.50, 's'),
+                'time_6_10': (1, None, 0.50, 's'),
+                'time_6_10000': (1, None, 0.50, 's'),
+                'time_6_1000000': (1, None, 0.50, 's')
+            },
+            'ubelix:amd': {
+                'time_2_10': (2e-04, None, 0.50, 's'),
+                'time_2_10000': (1e-03, None, 0.50, 's'),
+                'time_2_1000000': (5e-03, None, 0.50, 's'),
+                'time_4_10': (9e-04, None, 0.50, 's'),
+                'time_4_10000': (1e-03, None, 0.50, 's'),
+                'time_4_1000000': (1e-01, None, 0.50, 's'),
+                #'time_6_10': (1e-04, None, 0.50, 's'),
+                #'time_6_10000': (1e-03, None, 0.50, 's'),
+                #'time_6_1000000': (1e-02, None, 0.50, 's')
+            },
+        }
+
+        self.maintainers = ['Mandes']
+        self.tags = {'benchmark'}
